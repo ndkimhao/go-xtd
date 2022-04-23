2a6548297ffc53af69f3e6b5135956e2b23d08c8
KimHao Nguyen <ndkimhao@users.noreply.github.com>
2022-04-22 23:38:14 -0500

    xrand dieharder

---
+++ xrand/dieharder_test/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/binary"
+	"os"
+	"strconv"
+
+	"github.com/ndkimhao/go-xtd/fastrng"
+)
+
+func main() {
+	strSeed := "0"
+	if len(os.Args) >= 2 {
+		strSeed = os.Args[1]
+	}
+	seed, err := strconv.ParseInt(strSeed, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+	raw := len(os.Args) >= 3 && os.Args[2] == "-raw"
+	var seed32 [32]byte
+	binary.LittleEndian.PutUint64(seed32[:], uint64(seed))
+	rng := fastrng.New(seed32)
+	var buf [499]byte
+	var ibuf [8]byte
+	for {
+		if raw {
+			_, _ = rng.Read(buf[:])
+			_, _ = os.Stdout.Write(buf[:])
+		} else {
+			binary.LittleEndian.PutUint64(ibuf[:], rng.Uint64())
+			_, _ = os.Stdout.Write(ibuf[:])
+		}
+	}
+}
