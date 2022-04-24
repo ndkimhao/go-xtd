12c72187dfd7887a7e24e103e47386f107913f73
KimHao Nguyen <ndkimhao@users.noreply.github.com>
2022-04-24 09:19:05 -0500

    lower bound

--- algo/binary_search.go
+++ algo/binary_search.go
@@ -30,11 +30,26 @@ func UpperBoundComp[T any, It iter.ConstRandomIterator[T, It]](first, last It, v
 	return first
 }
 
-//
-//func LowerBound[T constraints.Ordered, It iter.ConstRandomIterator[T, It]](first, last It, value T) It {
-//	return LowerBoundComp(first, last, value, xfn.LessComparatorOf[T])
-//}
-//
-//func LowerBoundComp[T any, It iter.ConstRandomIterator[T, It]](first, last It, value T, comp xfn.LessComparator[T]) It {
-//
-//}
+func LowerBound[T constraints.Ordered, It iter.ConstRandomIterator[T, It]](first, last It, value T) It {
+	return LowerBoundComp(first, last, value, xfn.LessComparatorOf[T])
+}
+
+func LowerBoundComp[T any, It iter.ConstRandomIterator[T, It]](first, last It, value T, comp xfn.LessComparator[T]) It {
+	count := iter.Distance(first, last)
+	if count < 0 {
+		panic("invalid range")
+	}
+	for count > 0 {
+		it := first
+		step := count / 2
+		it = it.Add(step)
+		if comp(it.Value(), value) {
+			it = it.Next()
+			first = it
+			count -= step + 1
+		} else {
+			count = step
+		}
+	}
+	return first
+}
--- algo/binary_search_test.go
+++ algo/binary_search_test.go
@@ -19,13 +19,10 @@ func TestUpperBound(t *testing.T) {
 		check(0, 0)
 		check(2, 1)
 		check(4, 2)
-
 		check(5, 3)
 		check(5, 4)
-
 		check(6, 5)
 		check(6, 6)
-
 		check(7, 7)
 		check(7, 8)
 	})
@@ -38,13 +35,10 @@ func TestUpperBound(t *testing.T) {
 		check(6, 0)
 		check(4, 1)
 		check(2, 2)
-
 		check(1, 3)
 		check(1, 4)
-
 		check(0, 5)
 		check(0, 6)
-
 		check(-1, 7)
 		check(-1, 8)
 	})
@@ -55,5 +49,48 @@ func TestUpperBound(t *testing.T) {
 	t.Run("Empty Reversed", func(t *testing.T) {
 		s := slice.Of[int]()
 		require.Equal(t, s.REnd(), algo.UpperBound(s.RBegin(), s.REnd(), 1))
+	})
+}
+
+func TestLowerBound(t *testing.T) {
+	t.Run("Normal", func(t *testing.T) {
+		//            0  1  2  3  4  5  6
+		s := slice.Of(1, 1, 2, 2, 3, 5, 7)
+		check := func(pos, val int) {
+			require.Equal(t, s.Begin().Add(pos), algo.LowerBound(s.Begin(), s.End(), val))
+		}
+		check(0, 0)
+		check(0, 1)
+		check(2, 2)
+		check(4, 3)
+		check(5, 4)
+		check(5, 5)
+		check(6, 6)
+		check(6, 7)
+		check(7, 8)
+	})
+	t.Run("Reversed", func(t *testing.T) {
+		//            0  1  2  3  4  5  6
+		s := slice.Of(7, 5, 3, 2, 2, 1, 1)
+		check := func(pos, val int) {
+			require.Equal(t, s.Begin().Add(pos+1), algo.LowerBound(s.RBegin(), s.REnd(), val).Base())
+		}
+		check(6, 0)
+		check(6, 1)
+		check(4, 2)
+		check(2, 3)
+		check(1, 4)
+		check(1, 5)
+		check(0, 6)
+		check(0, 7)
+		check(-1, 8)
+	})
+	t.Run("Empty", func(t *testing.T) {
+		s := slice.Of[int]()
+		require.Equal(t, s.End(), algo.LowerBound(s.Begin(), s.End(), 1))
+	})
+	t.Run("Empty Reversed", func(t *testing.T) {
+		s := slice.Of[int]()
+		require.Equal(t, s.REnd(), algo.LowerBound(s.RBegin(), s.REnd(), 1))
 	})
 }
