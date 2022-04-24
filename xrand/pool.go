8b992a645437e25f2bd8882f0f1e8df8c923a768
KimHao Nguyen <ndkimhao@users.noreply.github.com>
2022-04-23 21:43:23 -0500

    xrand

--- xrand/pool.go
+++ xrand/pool.go
@@ -1,33 +1,21 @@
 package xrand
 
 import (
-	crypto_rand "crypto/rand"
 	"math/rand"
 	"sync"
-
-	"github.com/ndkimhao/go-xtd/fastrng"
 )
 
 var (
 	_rngPool = sync.Pool{New: newRng}
 
 	_masterRngLock sync.Mutex
-	_masterRng     = func() *fastrng.RNG {
-		var b [32]byte
-		_, err := crypto_rand.Read(b[:])
-		if err != nil {
-			panic("cannot read from crypto/rand")
-		}
-		return fastrng.New(b)
-	}()
+	_masterRng     = rand.NewSource(Crypto().Int63())
 )
 
 func newRng() any {
 	_masterRngLock.Lock()
 	defer _masterRngLock.Unlock()
-	var b [32]byte
-	_, _ = _masterRng.Read(b[:])
-	return rand.New(fastrng.New(b))
+	return rand.New(rand.NewSource(_masterRng.Int63()))
 }
 
 func Get() *rand.Rand {
--- xrand/pool_test.go
+++ xrand/pool_test.go
@@ -1,39 +1,14 @@
 package xrand_test
 
 import (
-	cryto_rand "crypto/rand"
-	"encoding/binary"
 	math_rand "math/rand"
 	"runtime"
 	"testing"
 
-	"github.com/ndkimhao/go-xtd/fastrng"
 	"github.com/ndkimhao/go-xtd/xrand"
 )
 
-func BenchmarkMath_Uint64(b *testing.B) {
-	b.RunParallel(func(pb *testing.PB) {
-		v := uint64(0)
-		for pb.Next() {
-			v += math_rand.Uint64()
-		}
-		runtime.KeepAlive(&v)
-	})
-}
-
-func BenchmarkCrypto_Uint64(b *testing.B) {
-	b.RunParallel(func(pb *testing.PB) {
-		v := uint64(0)
-		for pb.Next() {
-			var b [8]byte
-			_, _ = cryto_rand.Read(b[:])
-			v += binary.LittleEndian.Uint64(b[:])
-		}
-		runtime.KeepAlive(&v)
-	})
-}
-
-func BenchmarkFRand_Uint64(b *testing.B) {
+func BenchmarkUint64(b *testing.B) {
 	b.RunParallel(func(pb *testing.PB) {
 		v := uint64(0)
 		for pb.Next() {
@@ -43,9 +18,9 @@ func BenchmarkFRand_Uint64(b *testing.B) {
 	})
 }
 
-func BenchmarkMathLocal_Uint64(b *testing.B) {
+func BenchmarkUint64_Local(b *testing.B) {
 	b.RunParallel(func(pb *testing.PB) {
-		rng := math_rand.New(math_rand.NewSource(1))
+		rng := math_rand.New(math_rand.NewSource(0))
 		v := uint64(0)
 		for pb.Next() {
 			v += rng.Uint64()
@@ -54,12 +29,11 @@ func BenchmarkMathLocal_Uint64(b *testing.B) {
 	})
 }
 
-func BenchmarkFRandLocal_Uint64(b *testing.B) {
+func BenchmarkUint64_Mutex(b *testing.B) {
 	b.RunParallel(func(pb *testing.PB) {
-		rng := math_rand.New(fastrng.New([32]byte{1, 2, 3}))
 		v := uint64(0)
 		for pb.Next() {
-			v += rng.Uint64()
+			v += math_rand.Uint64()
 		}
 		runtime.KeepAlive(&v)
 	})
